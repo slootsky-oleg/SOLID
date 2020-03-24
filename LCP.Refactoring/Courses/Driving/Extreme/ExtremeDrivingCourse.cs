1f1887467d5c08337f915e9a09a463136623fb22
Olegs <Olegs@OLEG-LAP>
2020-03-24 21:27:52 +0200

    Extreme driving course example

---
+++ LCP.Refactoring/Courses/Driving/Extreme/ExtremeDrivingCourse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using LCP.Courses.Shooting;
+
+namespace LCP.Courses.Driving.Extreme
+{
+    public class ExtremeDrivingCourse
+    {
+        private readonly PracticeDrivingCourse drivingCourse;
+        private Dictionary<DrivingTrainee, string> traineeWills;
+
+
+        public ExtremeDrivingCourse()
+        {
+            drivingCourse = new PracticeDrivingCourse
+            {
+                MinAge = 27,
+                MaxAge = 60
+            };
+
+            traineeWills = new Dictionary<DrivingTrainee, string>();
+
+        }
+
+
+        public void Enroll(DrivingTrainee trainee, string will)
+        {
+            traineeWills.Add(trainee, will);
+            drivingCourse.Enroll(trainee);
+        }
+
+        public void Complete()
+        {
+            drivingCourse.Complete();
+        }
+    }
+}
---
+++ LCP.Refactoring/Demo.cs
@@ -0,0 +1,57 @@
+﻿using LCP.Courses.Driving;
+using LCP.Courses.Driving.Extreme;
+using LCP.Courses.Shooting;
+
+namespace LCP
+{
+    public class Demo
+    {
+        public void Run()
+        {
+            RunShooting();
+
+            RunDriving();
+
+            RunExtremeDriving();
+        }
+
+        private static void RunExtremeDriving()
+        {
+            var driver1 = new DrivingTrainee();
+            var driver1Will = "my will";
+
+            var driver2 = new DrivingTrainee();
+            var driver2Will = "my will";
+
+            var extremeDriving = new ExtremeDrivingCourse();
+            extremeDriving.Enroll(driver1, driver1Will);
+            extremeDriving.Enroll(driver2, driver2Will);
+
+            extremeDriving.Complete();
+        }
+
+        private static void RunShooting()
+        {
+            var shooter1 = new ShootingTrainee();
+            var shooter2 = new ShootingTrainee();
+
+            var shooting = new PracticeShootingCourse();
+            shooting.Enroll(shooter1);
+            shooting.Enroll(shooter2);
+
+            shooting.Complete();
+        }
+
+        private static void RunDriving()
+        {
+            var driver1 = new DrivingTrainee();
+            var driver2 = new DrivingTrainee();
+
+            var driving = new PracticeDrivingCourse();
+            driving.Enroll(driver1);
+            driving.Enroll(driver2);
+
+            driving.Complete();
+        }
+    }
+}
